0bdca7fc8a6691211189368cf7aa557cad49bfa9
Turbasu Chatterjee <turbasu.chatterjee@gmail.com>
2022-04-08 12:42:03 +0530

    Added shapes

--- dot_product.py
+++ dot_product.py
@@ -1,8 +1,8 @@
 import utils
 import numpy as np
 
-g22 = np.memmap(filename='g22.npy', mode='r')
-g21 = np.memmap(filename='g21.npy', mode='r')
+g22 = np.memmap(filename='g22.npy', mode='r', shape=(27648,27648))
+g21 = np.memmap(filename='g21.npy', mode='r', shape=(27648,27648))
 
 r1 = utils.blockwise_dot(g22,g21)
 np.save('final_products/r1', r1)
